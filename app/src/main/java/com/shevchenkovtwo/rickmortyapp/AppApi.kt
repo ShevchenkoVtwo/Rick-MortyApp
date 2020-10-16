6dc525f5ff96f296d8b8ca10e61f5be50a1ad93e
vladimir4999 <shevchenkovtwo@gmail.com>
2020-10-16 14:33:43 +0300

    Added functionality

--- app/src/main/java/com/shevchenkovtwo/rickmortyapp/AppApi.kt
+++ app/src/main/java/com/shevchenkovtwo/rickmortyapp/AppApi.kt
@@ -1,10 +1,18 @@
 package com.shevchenkovtwo.rickmortyapp
 
-import com.shevchenkovtwo.rickmortyapp.model.CharactersResponse
+import com.shevchenkovtwo.rickmortyapp.responses.CharactersResponse
+import com.shevchenkovtwo.rickmortyapp.responses.EpisodesResponse
+import com.shevchenkovtwo.rickmortyapp.responses.LocationsResponse
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface AppApi {
     @GET("character/")
-    suspend fun getAllCharacters():Response<CharactersResponse>
+    suspend fun getAllCharacters(): Response<CharactersResponse>
+
+    @GET("location/")
+    suspend fun getAllLocations(): Response<LocationsResponse>
+
+    @GET("episode/")
+    suspend fun getAllEpisodes(): Response<EpisodesResponse>
 }
