7ef83fbb018ec87557c5f72675249649fb2a01ad
vladimir4999 <shevchenkovtwo@gmail.com>
2020-10-16 14:33:10 +0300

    Added ViewModel for episodes and locations

--- app/src/main/java/com/shevchenkovtwo/rickmortyapp/viewmodel/EpisodesViewModel.kt
+++ app/src/main/java/com/shevchenkovtwo/rickmortyapp/viewmodel/EpisodesViewModel.kt
@@ -1,7 +1,35 @@
 package com.shevchenkovtwo.rickmortyapp.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.gson.GsonBuilder
+import com.shevchenkovtwo.rickmortyapp.AppApi
+import com.shevchenkovtwo.rickmortyapp.AppConstants
+import com.shevchenkovtwo.rickmortyapp.model.Episode
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class EpisodesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val episodesData: MutableLiveData<List<Episode>> = MutableLiveData()
+
+    init {
+        val gson = GsonBuilder()
+            .setLenient()
+            .create()
+        val retrofit = Retrofit.Builder()
+            .baseUrl(AppConstants.baseUri)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+        val api = retrofit.create(AppApi::class.java)
+
+        viewModelScope.launch {
+            val result = api.getAllEpisodes()
+            result.body()?.episodes?.let {
+                episodesData.postValue(it)
+            }
+
+        }
+    }
 }
--- app/src/main/java/com/shevchenkovtwo/rickmortyapp/viewmodel/LocationViewModel.kt
+++ app/src/main/java/com/shevchenkovtwo/rickmortyapp/viewmodel/LocationViewModel.kt
@@ -1,7 +1,35 @@
 package com.shevchenkovtwo.rickmortyapp.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.gson.GsonBuilder
+import com.shevchenkovtwo.rickmortyapp.AppApi
+import com.shevchenkovtwo.rickmortyapp.AppConstants
+import com.shevchenkovtwo.rickmortyapp.model.Location
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class LocationViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val locationsData: MutableLiveData<List<Location>> = MutableLiveData()
+
+    init {
+        val gson = GsonBuilder()
+            .setLenient()
+            .create()
+        val retrofit = Retrofit.Builder()
+            .baseUrl(AppConstants.baseUri)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+        val api = retrofit.create(AppApi::class.java)
+
+        viewModelScope.launch {
+            val result = api.getAllLocations()
+            result.body()?.locations?.let {
+                locationsData.postValue(it)
+            }
+
+        }
+    }
 }
