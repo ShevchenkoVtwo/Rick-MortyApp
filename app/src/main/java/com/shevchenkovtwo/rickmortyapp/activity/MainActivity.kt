6882b518d16a4af4fa07700dc0a37a64e2d06cf6
vladimir4999 <shevchenkovtwo@gmail.com>
2020-10-17 11:03:35 +0300

    Fixed Navigation with https://github.com/android/architecture-components-samples.

--- app/src/main/java/com/shevchenkovtwo/rickmortyapp/activity/MainActivity.kt
+++ app/src/main/java/com/shevchenkovtwo/rickmortyapp/activity/MainActivity.kt
@@ -1,26 +1,51 @@
 package com.shevchenkovtwo.rickmortyapp.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.shevchenkovtwo.rickmortyapp.R
+import com.shevchenkovtwo.rickmortyapp.navigation.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentNavController: LiveData<NavController>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
-        val navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration:AppBarConfiguration = AppBarConfiguration(setOf(
-            R.id.menu_characters,R.id.menu_locations,R.id.menu_episodes
-        ))
-        setupActionBarWithNavController(navController,appBarConfiguration)
-        navView.setupWithNavController(navController)
+        if (savedInstanceState == null) {
+            setupBottomNavigationBar()
+        }
     }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupBottomNavigationBar()
+    }
+
+    private fun setupBottomNavigationBar() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        val navGraphIds =
+            listOf(R.navigation.characters, R.navigation.locations, R.navigation.episodes)
+
+        // Setup the bottom navigation view with a list of navigation graphs
+        val controller = bottomNavigationView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_fragment,
+            intent = intent
+        )
+
+        // Whenever the selected controller changes, setup the action bar.
+        controller.observe(this, Observer { navController ->
+            setupActionBarWithNavController(navController)
+        })
+        currentNavController = controller
+    }
+
 }
