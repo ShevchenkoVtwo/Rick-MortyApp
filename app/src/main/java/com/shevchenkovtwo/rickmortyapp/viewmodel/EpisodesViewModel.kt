2d2b51eb11a83313dd30210bb75d220ba44f8df7
vladimir4999 <shevchenkovtwo@gmail.com>
2020-10-21 12:32:24 +0300

    Reworked viewmodels

--- app/src/main/java/com/shevchenkovtwo/rickmortyapp/viewmodel/EpisodesViewModel.kt
+++ app/src/main/java/com/shevchenkovtwo/rickmortyapp/viewmodel/EpisodesViewModel.kt
@@ -5,14 +5,18 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.shevchenkovtwo.rickmortyapp.AppConstants
 import com.shevchenkovtwo.rickmortyapp.loadEpisodes
+import com.shevchenkovtwo.rickmortyapp.loadPages
 import com.shevchenkovtwo.rickmortyapp.model.Episode
+import com.shevchenkovtwo.rickmortyapp.model.Info
 import kotlinx.coroutines.launch
 
 class EpisodesViewModel : ViewModel() {
     val episodesData: MutableLiveData<List<Episode>> = MutableLiveData()
+    private var info: Info? = null
 
     init {
         viewModelScope.launch {
+            info = loadPages("episode")
             episodesData.value = loadEpisodes(AppConstants.pageCounter)
         }
     }
--- app/src/main/java/com/shevchenkovtwo/rickmortyapp/viewmodel/LocationsViewModel.kt
+++ app/src/main/java/com/shevchenkovtwo/rickmortyapp/viewmodel/LocationsViewModel.kt
@@ -3,33 +3,19 @@ package com.shevchenkovtwo.rickmortyapp.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.gson.GsonBuilder
-import com.shevchenkovtwo.rickmortyapp.AppRetrofitApiResponces
-import com.shevchenkovtwo.rickmortyapp.AppConstants
+import com.shevchenkovtwo.rickmortyapp.*
+import com.shevchenkovtwo.rickmortyapp.model.Info
 import com.shevchenkovtwo.rickmortyapp.model.Location
 import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class LocationsViewModel : ViewModel() {
     val locationsData: MutableLiveData<List<Location>> = MutableLiveData()
+    private var info: Info? = null
 
     init {
-        val gson = GsonBuilder()
-            .setLenient()
-            .create()
-        val retrofit = Retrofit.Builder()
-            .baseUrl(AppConstants.baseUri)
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .build()
-        val api = retrofit.create(AppRetrofitApiResponces::class.java)
-
         viewModelScope.launch {
-            val result = api.getAllLocations(1)
-            result.body()?.locations?.let {
-                locationsData.postValue(it)
-            }
-
+            info = loadPages("location")
+            locationsData.value = loadLocations(AppConstants.pageCounter)
         }
     }
 }
