1bd213cb1ac707dd17a1add06fd78be29b85102d
vladimir4999 <shevchenkovtwo@gmail.com>
2020-10-21 12:33:15 +0300

    Implemented ViewModel

--- app/src/main/java/com/shevchenkovtwo/rickmortyapp/viewmodel/EpisodeViewModel.kt
+++ app/src/main/java/com/shevchenkovtwo/rickmortyapp/viewmodel/EpisodeViewModel.kt
@@ -1,7 +1,21 @@
 package com.shevchenkovtwo.rickmortyapp.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.shevchenkovtwo.rickmortyapp.AppConstants
+import com.shevchenkovtwo.rickmortyapp.model.Episode
 
 class EpisodeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    var episodeData: MutableLiveData<Episode>? = null
+
+    init {
+        episodeData = MutableLiveData()
+    }
+
+    fun getEpisodeLiveData(): MutableLiveData<Episode> {
+        if (episodeData == null)
+            episodeData = MutableLiveData()
+        episodeData?.value = AppConstants.episodeSelected
+        return episodeData!!
+    }
 }
--- app/src/main/java/com/shevchenkovtwo/rickmortyapp/viewmodel/LocationViewModel.kt
+++ app/src/main/java/com/shevchenkovtwo/rickmortyapp/viewmodel/LocationViewModel.kt
@@ -1,7 +1,21 @@
 package com.shevchenkovtwo.rickmortyapp.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.shevchenkovtwo.rickmortyapp.AppConstants
+import com.shevchenkovtwo.rickmortyapp.model.Location
 
 class LocationViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    var locationData: MutableLiveData<Location>? = null
+
+    init {
+        locationData = MutableLiveData()
+    }
+
+    fun getLocationLiveData(): MutableLiveData<Location> {
+        if (locationData == null)
+            locationData = MutableLiveData()
+        locationData?.value = AppConstants.locationSelected
+        return locationData!!
+    }
 }
